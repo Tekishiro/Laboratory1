bbedf55230226ad0df70ad00c9e42014cb7a8840
Tekishiro <tekishiromail@gmail.com>
2015-09-30 20:48:23 +0300

    Созданы классы третьей лаборторной работы.

---
+++ LaboratoryWork3/LaboratoryWork3/Classes.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "stdafx.h"
+#include <iostream>
+//#include <string>
+//#include <string.h>
+using namespace std;
+
+//Классы – Тариф, Корпоративный, Индивидульный, Стандарт, Бизнес Про и т.д.
+
+class Tarif //тариф имеет абонентскую плату
+{
+private:
+	int fee;
+
+public:
+	Tarif() { cout << "Тариф создан!" << endl; }
+	~Tarif() { cout << "Тариф удален!" << endl; }
+
+	void SetFee(int);
+	void GetFee();
+};
+
+
+class Corp : public Tarif //корпоративный тариф - бесплатные звонки коллегам
+{
+private:
+	unsigned int associate;
+
+public:
+	Corp() { cout << "Корпоративный тариф создан!" << endl; }
+	~Corp() { cout << "Корпоративный тариф удален!" << endl; }
+
+	void SetAssociate(int);
+	void GetAssociate();
+};
+
+class Individual : public Tarif //индивидуальный тариф - бесплатные минуты
+{
+private:
+	unsigned int freemins;
+
+public:
+	Individual() { cout << "Индивидуальный тариф создан!" << endl; }
+	~Individual() { cout << "Индивидуальный тариф удален!" << endl; }
+
+	void SetFreeMins(int);
+	void GetFreeMins();
+};
+
+class Standart : public Tarif
+{
+private:
+	unsigned int freesms;
+
+public:
+	Standart() { cout << "Стандартный тариф создан!" << endl; }
+	~Standart() { cout << "Стандартный тариф удален!" << endl; }
+
+	void SetFreeSms(int);
+	void GetFreeSms();
+};
+
+class BusinessPRO : public Tarif //бесплатные мегабайты интернета
+{
+private:
+	unsigned int megabytes;
+
+public:
+	BusinessPRO() { cout << "Тариф БизнесПРО создан!" << endl; }
+	~BusinessPRO() { cout << "Тариф БизнесПРО удален!" << endl; }
+
+	void SetMegabytes(int);
+	void GetMegabytes();
+};
